5c2154d50de87da99d4ed3f9bd97939ed923913c
明动华夏 <524981259@qq.com>
2016-05-31 20:06:27 +0800

    新建项目

---
+++ GroupWork/GroupWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GroupWork
+//
+//  Created by 徐明明 on 16/5/31.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GroupWork/GroupWork/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GroupWork
+//
+//  Created by 徐明明 on 16/5/31.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"项目初建立");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
