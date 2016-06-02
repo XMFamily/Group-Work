9595460be2fb244ab82c28d8007e6473c4d825ac
徐明明 <huamingming@163.com>
2016-06-02 12:54:11 +0800

    添加TabBar的第一个子控制器

--- GroupWork/GroupWork/XMTabBarController.m
+++ GroupWork/GroupWork/XMTabBarController.m
@@ -7,6 +7,7 @@
 //
 
 #import "XMTabBarController.h"
+#import "XMNavigationController.h"
 
 @interface XMTabBarController ()
 
@@ -22,7 +23,36 @@
 #pragma mark - 设置界面
 - (void)setupUI {
     self.view.backgroundColor= [UIColor whiteColor];
+    
+    NSMutableArray *arrayM = [NSMutableArray array];
+    
+    [arrayM addObject:[self viewControllerWithClsName:@"ViewController" titleName:@"我的" imageName:@"TabBar_Assets"]];
+    
+    
+    self.viewControllers = arrayM;
 }
+
+
+- (UIViewController *)viewControllerWithClsName:(NSString *)clsName titleName:(NSString *)title imageName:(NSString *)imageName {
+    
+    Class cls = NSClassFromString(clsName);
+    
+    UIViewController *vc = [[cls alloc] init];
+    
+    vc.tabBarItem.image = [UIImage imageNamed:imageName];
+    UIImage *imageSelected = [UIImage imageNamed:[imageName stringByAppendingFormat:@"_Sel"]];
+    
+    vc.tabBarItem.selectedImage = [imageSelected imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+    
+    vc.title = title;
+    
+    XMNavigationController *nc = [[XMNavigationController alloc] initWithRootViewController:vc];
+    
+    return nc;
+    
+}
+
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
