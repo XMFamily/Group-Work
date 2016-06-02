adcacd9fb66278015c28d690df5c876070aa9aff
徐明明 <huamingming@163.com>
2016-06-02 12:59:30 +0800

    创建子控制器

---
+++ GroupWork/GroupWork/ZFBFriendsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZFBFriendsViewController.h
+//  GroupWork
+//
+//  Created by 徐明明 on 16/6/2.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZFBFriendsViewController : UIViewController
+
+@end
