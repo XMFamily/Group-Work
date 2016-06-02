e65dd3fbc46c55abdd7139e62b2ddc86625c39f5
許仙 <xuxiandaguanren@gmail.com>
2016-06-02 23:55:21 +0800

    修改项目前缀 创建常用功能按钮类 导入到支付宝页面

---
+++ GroupWork/GroupWork/ZFBCommonFunctionView.h
@@ -0,0 +1,13 @@
+//
+//  ZFBCommonFunctionView.h
+//  GroupWork
+//
+//  Created by macssss on 16/6/2.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZFBCommonFunctionView : UIView
+
+@end
---
+++ GroupWork/GroupWork/ZFBCommonFunctionView.m
@@ -0,0 +1,21 @@
+//
+//  ZFBCommonFunctionView.m
+//  GroupWork
+//
+//  Created by macssss on 16/6/2.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import "ZFBCommonFunctionView.h"
+
+@implementation ZFBCommonFunctionView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
